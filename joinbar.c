e901df787f1d68d9dac55c083b79234545bc7711
Dušan Baran <work.dusanbaran@gmail.com>
2019-11-05 11:46:45 +0100

    Add memory refcount handling

--- joinbar.c
+++ joinbar.c
@@ -1,12 +1,15 @@
 #define _POSIX_C_SOURCE 2
 
-#include <stdio.h>
 #include <unistd.h>
 #include <poll.h>
 
 #include <json-c/json.h>
 
 #define BUFFER_SIZE 1024
+#define VERSION_STR "{\"version\":1,\"click_events\":true}\n"
+#define PREFIX_STR "[\n"
+#define POSTFIX_STR "]\n"
+#define SEPARATOR_STR ",\n"
 
 static int r_status(const char *scmd) {
   int pfd[2];
@@ -53,7 +56,7 @@ static json_object* merge(json_object **jss, size_t njss) {
 
     for (j = 0; j < json_object_array_length(jss[i]); ++j) {
       e = json_object_array_get_idx(jss[i], j);
-      json_object_array_add(r, e);
+      json_object_array_add(r, json_object_get(e));
     }
   }
 
@@ -61,21 +64,28 @@ static json_object* merge(json_object **jss, size_t njss) {
 }
 
 static void on_status(json_object **jo, int fd) {
-  char buf[BUFFER_SIZE];
+  char buf[BUFFER_SIZE] = {0};
   json_object *t;
 
   read(fd, buf, BUFFER_SIZE);
   t = json_tokener_parse(buf[0] == ',' ? buf + 1 : buf);
 
-  if (json_object_is_type(t, json_type_array))
+  if (json_object_is_type(t, json_type_array)) {
+    if (*jo) json_object_put(*jo);
     *jo = t;
+  }
 }
 
 int main(int argc, char **argv) {
   struct pollfd fds[argc - 1];
   json_object *fjo[argc - 1];
+  json_object *rjo = NULL;
+  const char *json = NULL;
   int i;
-  json_object *rjo;
+  size_t l;
+
+  write(0, VERSION_STR, sizeof(VERSION_STR));
+  write(0, PREFIX_STR, sizeof(PREFIX_STR));
 
   for (i = 0; i < argc - 1; ++i)
     fjo[i] = NULL;
@@ -85,8 +95,6 @@ int main(int argc, char **argv) {
     fds[i].events = POLLIN;
   }
 
-  printf("{\"version\":1}\n");
-  printf("[\n");
   while (argc > 1) {
     if (poll(fds, argc - 1, -1) <= 0) continue;
 
@@ -97,9 +105,13 @@ int main(int argc, char **argv) {
     }
 
     rjo = merge(fjo, argc - 1);
-    printf("%s\n,\n", json_object_get_string(rjo));
-    fflush(stdout);
+    json = json_object_to_json_string_length(rjo, JSON_C_TO_STRING_PLAIN, &l);
+    write(0, json, l);
+    write(0, SEPARATOR_STR, sizeof(SEPARATOR_STR));
+    json_object_put(rjo);
   }
+
+  write(0, POSTFIX_STR, sizeof(POSTFIX_STR));
 
   return 0;
 }
